53bee4146922ed6d6c52b2b90aca20ac1a9e71ed
johan martinsson <jm1974@hotmail.com>
2019-02-24 22:27:37 +0100

    night pass

--- typescript/src/prices.ts
+++ typescript/src/prices.ts
@@ -4,11 +4,11 @@ import mysql from "mysql2/promise"
 async function createApp() {
     const app = express()
 
-    let connectionOptions = {host:'localhost', user: 'root', database: 'test', password: 'mysql'}
+    let connectionOptions = {host: 'localhost', user: 'root', database: 'test', password: 'mysql'}
     const connection = await mysql.createConnection(connectionOptions);
 
     app.put('/prices', async (req, res) => {
-        const liftPassCost = req.query.cost//?
+        const liftPassCost = req.query.cost
         const liftPassType = req.query.type
         const [rows, fields] = await connection.execute(
             'INSERT INTO `liftpass` (type, cost) VALUES (?, ?) ' +
@@ -24,10 +24,19 @@ async function createApp() {
             'SELECT cost FROM `liftpass` ' +
             'WHERE `type` = ? ',
             [liftPassType])
+        if (liftPassType === 'night') {
+            if (age < 6) {
+                res.send({cost: 0})
+            } else if (age > 74) {
+                res.send({cost: Math.ceil(result.cost * .42)})
+            } else {
+                res.send(result)
+            }
+            return
+        }
         if (age < 6) {
             res.send({cost: 0})
-        }
-        else if (age < 15) {
+        } else if (age < 15) {
             res.send({cost: Math.ceil(result.cost * .7)})
         } else if (age > 74) {
             res.send({cost: Math.ceil(result.cost * .42)})
--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -6,15 +6,17 @@ describe('prices', () => {
     let app, connection
     beforeEach(async function () {
         ({app, connection}  = await createApp())
-        let url = toUrl('/prices', {type: '1jour', cost: 35})
         await request(app)
-            .put(url)
+            .put(toUrl('/prices', {type: '1jour', cost: 35}))
+            .expect(200)
+        await request(app)
+            .put(toUrl('/prices', {type: 'night', cost: 19}))
             .expect(200)
     });
     afterEach(async function () {
         await connection.close();
     });
-    
+
     it('the 1 day full price pass is the standard', async () => {
 
         await obtainPrices({type: '1jour'})
@@ -28,15 +30,35 @@ describe('prices', () => {
         [5, 0],
         [65, 27],
         [75, 15]
-    ].forEach(([age, cost]) => {
+    ].forEach(([age, expectedCost]) => {
         it('the 1 day children price pass is 30% off, rounded up', async () => {
             let params = {type: '1jour', age}
             await obtainPrices(params)
                 .expect( res => {
-                    expect(res.body).property('cost', cost)
+                    expect(res.body).property('cost', expectedCost)
                 })
         });
-    })
+    });
+
+
+    [
+        [25, 19],
+        [14, 19],
+        [65, 19],
+        [5, 0],
+        [75, 8]
+    ].forEach(([age, expectedCost]) => {
+        it('the night pass is 19 for everyone but very young and very old people', async () => {
+            let params = {type: 'night', age}
+            await obtainPrices(params)
+                .expect( res => {
+                    expect(res.body).property('cost', expectedCost)
+                })
+        });
+    });
+
+
+    // todo 2-4, and 5, 6 day pass
 
     function obtainPrices(params) {
         return request(app)
