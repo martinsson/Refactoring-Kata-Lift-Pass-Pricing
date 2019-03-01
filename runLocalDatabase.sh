b66958433acd5b2f5a428f855ef39a62dd00eba2
martinsson <martinsson.johan@gmail.com>
2019-03-01 21:27:02 +0100

    add comments to startup script

--- runLocalDatabase.sh
+++ runLocalDatabase.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker rm -f mariadb 2&> /dev/null
+
+# start mariadb and launch all sql scripts in the directory database
 docker run -p 3306:3306 --name mariadb -e MYSQL_ROOT_PASSWORD=mysql -d  -v $PWD/database:/docker-entrypoint-initdb.d mariadb:10.4
