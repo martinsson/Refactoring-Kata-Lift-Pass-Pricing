5279fdee5e07a6ca8a414f1238df9ac51886f06b
Peter Kofler <peter.kofler@code-cop.org>
2019-03-23 23:49:23 +0100

    Open DB connection at startup.
    Conflicts:
    	csharp/LiftPassPricing/Prices.cs

--- csharp/LiftPassPricingTests/PricesTest.cs
+++ csharp/LiftPassPricingTests/PricesTest.cs
@@ -8,13 +8,23 @@ namespace LiftPassPricingTests
     /// <seealso>"http://www.marcusoft.net/2013/01/NancyTesting1.html"</seealso>
     public class PricesTest
     {
+        private readonly Prices prices;
+
+        public PricesTest()
+        {
+            this.prices = new Prices();
+        }
+
+        public void Dispose()
+        {
+            prices.connection.Close();
+        }
 
         [Fact]
         public void Should_return_status_ok_when_route_exists()
         {
             // Given
-            // var bootstrapper = new DefaultNancyBootstrapper();
-            var browser = new Browser(with => with.Module(new Prices()));
+            var browser = new Browser(with => with.Module(prices));
 
             // When
             var result = browser.Get("/", with =>
