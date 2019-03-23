85aac04d733a8d723fbb5284e94c063e692d8ecc
Peter Kofler <peter.kofler@code-cop.org>
2019-03-23 23:15:42 +0100

    Nancy dummy module.

---
+++ csharp/LiftPassPricing/Prices.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace LiftPassPricing
+{
+    public class Prices : NancyModule
+    {
+
+        public Prices()
+        {
+            Get("/", args => "Hello from Nancy running on CoreCLR");
+        }
+
+    }
+}
