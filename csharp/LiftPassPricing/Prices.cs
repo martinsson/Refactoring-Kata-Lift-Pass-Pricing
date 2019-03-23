73ad008ca7efe3a54f82c18e95cad160d0b6e507
Peter Kofler <peter.kofler@code-cop.org>
2019-03-23 23:49:23 +0100

    Open DB connection at startup.
    Conflicts:
    	csharp/LiftPassPricingTests/PricesTest.cs

--- csharp/LiftPassPricing/Prices.cs
+++ csharp/LiftPassPricing/Prices.cs
@@ -1,12 +1,21 @@
 ﻿using Nancy;
+using MySql.Data.MySqlClient;
 
 namespace LiftPassPricing
 {
     public class Prices : NancyModule
     {
 
+        public readonly MySqlConnection connection;
+
         public Prices()
         {
+            this.connection = new MySqlConnection
+            {
+                ConnectionString = "Database=lift_pass;Data Source=localhost;User Id=root;Password=mysql"
+            };
+            connection.Open();
+
             Get("/", args => "Hello from Nancy running on CoreCLR");
         }
 
