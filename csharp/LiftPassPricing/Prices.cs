1b9810fd83d3596e369a5322172b11304fd4fe22
Peter Kofler <peter.kofler@code-cop.org>
2019-03-25 21:43:41 +0100

    PUT inserts prices.

--- csharp/LiftPassPricing/Prices.cs
+++ csharp/LiftPassPricing/Prices.cs
@@ -1,4 +1,5 @@
-﻿using Nancy;
+﻿using System;
+using Nancy;
 using MySql.Data.MySqlClient;
 
 namespace LiftPassPricing
@@ -16,7 +17,24 @@ namespace LiftPassPricing
             };
             connection.Open();
 
-            Get("/", args => "Hello from Nancy running on CoreCLR");
+            Put("/prices", (args) =>
+            {
+                int liftPassCost = Int32.Parse(this.Request.Query["cost"]);
+                string liftPassType = this.Request.Query["type"];
+
+                using (var command = new MySqlCommand( //
+                       "INSERT INTO base_price (type, cost) VALUES (@type, @cost) " + //
+                       "ON DUPLICATE KEY UPDATE cost = @cost;", connection))
+                {
+                    command.Parameters.AddWithValue("@type", liftPassType);
+                    command.Parameters.AddWithValue("@cost", liftPassCost);
+                    command.Prepare();
+                    command.ExecuteNonQuery();
+                }
+
+                return "";
+            });
+
         }
 
     }
