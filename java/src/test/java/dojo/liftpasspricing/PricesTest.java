0dd6501a9754c00214537fc2fc5df5eaf3b0fa6e
Peter Kofler <peter.kofler@code-cop.org>
2019-03-27 22:02:59 +0100

    Make Java test similar to Typescript, remove test names, reduce test cases.

--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -23,7 +24,7 @@ public class PricesTest {
     @BeforeEach
     public void createPrices() throws SQLException {
         connection = Prices.createApp();
-        
+
         createPrice("1jour", 35);
         createPrice("night", 19);
     }
@@ -47,52 +48,52 @@ public class PricesTest {
     }
 
     @Test
-    public void the_1_day_full_price_pass_is_the_standard() {
+    public void defaultCost() {
         JsonPath json = obtainPrice("type", "1jour");
         int cost = json.get("cost");
         assertEquals(35, cost);
     }
 
-    @ParameterizedTest(name = "the 1 day age {0} price pass is rounded up to {1}")
-    @CsvSource({ "5, 0",
-                 "6, 25",
-                 "14, 25",
-                 "15, 35",
-                 "25, 35", 
-                 "64, 35", 
-                 "65, 27", 
-                 "74, 27", 
-                 "75, 14"})
-    public void the_1_day_price_pass_is_rounded_up(int age, int expectedCost) {
+    @ParameterizedTest
+    @CsvSource({ "5, 0", //
+                 "6, 25", //
+                 "14, 25", //
+                 "15, 35", //
+                 "25, 35", //
+                 "64, 35", //
+                 "65, 27" })
+    public void worksForAllAges(int age, int expectedCost) {
         JsonPath json = obtainPrice("type", "1jour", "age", Integer.toString(age));
         int cost = json.get("cost");
         assertEquals(expectedCost, cost);
     }
 
+    @Disabled
+    @Test
+    public void defaultNightCost() {
+        JsonPath json = obtainPrice("type", "night");
+        int cost = json.get("cost");
+        assertEquals(19, cost);
+    }
+
     @ParameterizedTest
-    @CsvSource({ "5, 0", 
-                 "6, 19",
-                 "14, 19",
-                 "15, 19",
-                 "25, 19", 
-                 "64, 19", 
-                 "65, 19", 
-                 "74, 19", 
-                 "75, 8"})
-    public void the_night_pass_is_19_for_everyone_but_very_young_and_very_old_people(int age, int expectedCost){
+    @CsvSource({ "5, 0", //
+                 "6, 19", //
+                 "25, 19", //
+                 "64, 19", // 
+                 "65, 8" })
+    public void worksForNightPasses(int age, int expectedCost) {
         JsonPath json = obtainPrice("type", "night", "age", Integer.toString(age));
         int cost = json.get("cost");
         assertEquals(expectedCost, cost, "age " + age);
     }
 
-    // Monday x percent off
-    @ParameterizedTest(name = "Monday are 40 off, but not during holidays at {1}")
-    @CsvSource({ "25, '2019-02-24', 35", // Sunday, no deal
-                 "25, '2019-02-25', 35", // Monday holidays, no deal
-                 "25, '2019-03-28', 35", // not a Monday
-                 "25, '2019-03-25', 22", // ~40% off
-                 "65, '2019-03-25', 17"}) // ~40% off
-    public void mondays_are_40_off(int age, String date, int expectedCost){
+    @ParameterizedTest
+    @CsvSource({ "15, '2019-02-22', 35", // 
+                 "15, '2019-02-25', 35", //
+                 "15, '2019-03-11', 23", //
+                 "65, '2019-03-11', 18" })
+    public void mondays_are_40_off(int age, String date, int expectedCost) {
         JsonPath json = obtainPrice("type", "1jour", "age", Integer.toString(age), "date", date);
         int cost = json.get("cost");
         assertEquals(expectedCost, cost);
@@ -110,8 +111,7 @@ public class PricesTest {
     }
     
     private JsonPath obtainPrice(String... keyValue) {
-        RequestSpecification when = given().
-                                    when();
+        RequestSpecification when = given().when();
 
         for (int i = 0; i < keyValue.length; i += 2) {
             when = when.params(keyValue[i], keyValue[i + 1]);
