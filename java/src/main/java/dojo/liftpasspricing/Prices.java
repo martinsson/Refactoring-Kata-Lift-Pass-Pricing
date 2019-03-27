2fa348da4c1433c64655aa269cfeb400448c7d77
Peter Kofler <peter.kofler@code-cop.org>
2019-03-27 22:27:22 +0100

    Port simplification of code to Java.

--- java/src/main/java/dojo/liftpasspricing/Prices.java
+++ java/src/main/java/dojo/liftpasspricing/Prices.java
@@ -28,7 +28,7 @@ public class Prices {
 
             try (PreparedStatement stmt = connection.prepareStatement( //
                     "INSERT INTO base_price (type, cost) VALUES (?, ?) " + //
-            "ON DUPLICATE KEY UPDATE cost = ?")) {
+                    "ON DUPLICATE KEY UPDATE cost = ?")) {
                 stmt.setString(1, liftPassType);
                 stmt.setInt(2, liftPassCost);
                 stmt.setInt(3, liftPassCost);
@@ -39,21 +39,26 @@ public class Prices {
         });
 
         get("/prices", (req, res) -> {
-            try (PreparedStatement costStmt = connection.prepareStatement("SELECT cost FROM base_price WHERE type = ?")) {
+            try (PreparedStatement costStmt = connection.prepareStatement( //
+                    "SELECT cost FROM base_price " + //
+                    "WHERE type = ?")) {
                 costStmt.setString(1, req.queryParams("type"));
                 try (ResultSet result = costStmt.executeQuery()) {
                     result.next();
 
-                    int reduction = 0;
+                    int reduction;
                     boolean isHoliday = false;
 
                     if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) < 6) {
                         return "{ \"cost\": 0}";
                     } else {
+                        reduction = 0;
+                        
                         if (!req.queryParams("type").equals("night")) {
                             DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-                            try (PreparedStatement holidayStmt = connection.prepareStatement("SELECT * FROM holidays")) {
+                            try (PreparedStatement holidayStmt = connection.prepareStatement( //
+                                    "SELECT * FROM holidays")) {
                                 try (ResultSet holidays = holidayStmt.executeQuery()) {
 
                                     while (holidays.next()) {
@@ -75,7 +80,7 @@ public class Prices {
                                 Calendar calendar = Calendar.getInstance();
                                 calendar.setTime(isoFormat.parse(req.queryParams("date")));
                                 if (!isHoliday && calendar.get(Calendar.DAY_OF_WEEK) == 2) {
-                                    reduction = 60;
+                                    reduction = 35;
                                 }
                             }
 
@@ -83,36 +88,23 @@ public class Prices {
                             if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) < 15) {
                                 return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .7) + "}";
                             } else {
-                                if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) > 74) {
-                                    return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .4) + "}";
+                                if (req.queryParams("age") == null) {
+                                    double cost = result.getInt("cost") * (1 - reduction / 100.0);
+                                    return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
                                 } else {
-                                    if (req.queryParams("age") == null) {
-                                        double cost = result.getInt("cost");
-                                        if (reduction > 0) {
-                                            cost = cost / (1 + reduction / 100.0);
-                                        }
+                                    if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) > 64) {
+                                        double cost = result.getInt("cost") * .75 * (1 - reduction / 100.0);
                                         return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
                                     } else {
-                                        if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) > 64) {
-                                            double cost = result.getInt("cost") * .75;
-                                            if (reduction > 0) {
-                                                cost = cost / (1 + reduction / 100.0);
-                                            }
-                                            return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
-                                        } else {
-                                            double cost = result.getInt("cost");
-                                            if (reduction > 0) {
-                                                cost = cost / (1 + reduction / 100.0);
-                                            }
-                                            return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
-                                        }
+                                        double cost = result.getInt("cost") * (1 - reduction / 100.0);
+                                        return "{ \"cost\": " + (int) Math.ceil(cost) + "}";
                                     }
                                 }
                             }
                         } else {
                             if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) >= 6) {
-                                if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) > 74) {
-                                    return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") / 2.5) + "}";
+                                if (req.queryParams("age") != null && Integer.parseInt(req.queryParams("age")) > 64) {
+                                    return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .4) + "}";
                                 } else {
                                     return "{ \"cost\": " + result.getInt("cost") + "}";
                                 }
