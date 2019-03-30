0823b5600d301cab1385ab8367ab6de0eb9c6da5
Sébastian Le Merdy <sebastian.lemerdy@compte-nickel.fr>
2019-03-30 07:49:12 +0100

    conform to same TS test skeleton in scala

--- scala/src/main/scala/liftpasspricing/WebServer.scala
+++ scala/src/main/scala/liftpasspricing/WebServer.scala
@@ -7,6 +7,6 @@ object WebServer extends App {
       |you can open http://localhost:5010/prices?type=night&age=23&date=2019-02-18 in a navigator
       |and you'll get the price of the list pass for the day.""".stripMargin)
 
-  LiftPassPricing.startServer("localhost", 5010)
+  new LiftPassPricing().startServer("localhost", 5010)
 
 }
