a5f0f174147af4da8abfcbc1da7eb1dd278bc86d
Peter Kofler <peter.kofler@code-cop.org>
2019-04-14 21:25:46 +0200

    Create a test skeleton for Java.

---
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -0,0 +1,49 @@
+package dojo.liftpasspricing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import spark.Spark;
+
+public class PricesTest {
+
+    private Connection connection;
+
+    @BeforeEach
+    public void createPrices() throws SQLException {
+        connection = Prices.createApp();
+    }
+
+    @AfterEach
+    public void stopApplication() throws SQLException {
+        Spark.stop();
+        connection.close();
+    }
+
+    @Test
+    public void doesSomething() {
+        JsonPath response = RestAssured.
+            given().
+                port(4567).
+            when().
+                // construct some proper url parameters
+                get("/prices").
+            then().
+                assertThat().
+                    statusCode(200).
+                assertThat().
+                    contentType("application/json").
+            extract().jsonPath();
+
+        assertEquals("putSomehtingHere", response.get("putSomehtingHere"));
+    }
+
+}
