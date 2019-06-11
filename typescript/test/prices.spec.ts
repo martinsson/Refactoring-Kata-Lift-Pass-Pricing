b43c3a0aaebca9332b7e572faa94ef24d076404c
johan martinsson <jm1974@hotmail.com>
2019-06-11 12:45:40 +0200

    Revert "tests"
    This reverts commit cf2c740e0f3ff9149726b33d4c8a8433b75a4b6a.

--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -14,51 +14,13 @@ describe('prices', () => {
         connection.close()
     });
 
-    it('pass 1jour is €35', async () => {
+    it('does something', async () => {
 
         const response = await request(app)
             .get('/prices?type=1jour')
 
-        var expectedResult = {cost: 35} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
+        var exptectedResult = {cost: 35} // change this to make the test pass
+        expect(response.body).deep.equal(exptectedResult)
     });
-    it('pass night is free', async () => {
 
-        const response = await request(app)
-            .get('/prices?type=night')
-        var expectedResult = {cost: 0} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-    });
-    it('age under 6 is free', async () => {
-
-        const response = await request(app)
-            .get('/prices?age=5')
-
-        var expectedResult = {cost: 0} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-    });
-    it('age greater or equal to 6, type is required and price is 25', async () => {
-
-        const response = await request(app)
-            .get('/prices?age=6&type=1jour')
-        var expectedResult = {cost: 25} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-
-    });
-    it('age greater or equal to 15, type is required and price is 35', async () => {
-
-        const response = await request(app)
-            .get('/prices?age=15&type=1jour')
-        var expectedResult = {cost: 35} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-
-    });
-    it('age greater than 64, type is required and price is 27', async () => {
-
-        const response = await request(app)
-            .get('/prices?age=65&type=1jour')
-        var expectedResult = {cost: 27} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-
-    });
 });
