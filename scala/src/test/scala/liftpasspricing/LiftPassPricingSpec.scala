c531af63b91dce96a7d234c67329f29177c70d3f
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2022-03-28 07:00:13 +0200

    use FunSpec instead of FlatSpec
    By using FunSpec, test better looks like the original code base

--- scala/src/test/scala/liftpasspricing/LiftPassPricingSpec.scala
+++ scala/src/test/scala/liftpasspricing/LiftPassPricingSpec.scala
@@ -4,24 +4,29 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import liftpasspricing.JsonFormats._
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers._
 
 import scala.Function.const
 import scala.util.Using
 
-class LiftPassPricingSpec extends AnyFlatSpec with ScalatestRouteTest {
+class LiftPassPricingSpec extends AnyFunSpec with ScalatestRouteTest {
 
-  def withLiftPassPricing(testCode: Route => Any): Unit = {
-    val (routes, connection) = new LiftPassPricing().createApp()
-    Using(connection)(const(testCode(routes)))
-  }
+  describe("prices") {
 
-  it should "does something" in withLiftPassPricing { app =>
-    Get("/prices?type=1jour") ~> app ~> check {
+    def withPrices(testCode: Route => Any): Unit = {
+      val (routes, connection) = new LiftPassPricing().createApp()
+      Using(connection)(const(testCode(routes)))
+    }
 
-      val exptectedResult = Cost(35) // change this to make the test pass
-      responseAs[Cost] shouldBe exptectedResult
+    it("does something") {
+      withPrices { app =>
+        Get("/prices?type=1jour") ~> app ~> check {
+
+          val exptectedResult = Cost(35) // change this to make the test pass
+          responseAs[Cost] shouldBe exptectedResult
+        }
+      }
     }
   }
 
