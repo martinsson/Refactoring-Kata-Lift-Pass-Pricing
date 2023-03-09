0fbfe8c81819c5b395c5e3ad8aba5455c7fbff09
emily <emily@bacheconsulting.com>
2023-03-09 14:59:11 +0100

    wait for the server to start rather than sleeping a fixed time

--- python/test/test_prices.py
+++ python/test/test_prices.py
@@ -14,14 +14,24 @@ def server(port):
     app.run(port=port)
 
 
+def wait_for_server_to_start(server_url):
+    started = False
+    while not started:
+        try:
+            requests.get(server_url)
+            started = True
+        except Exception as e:
+            time.sleep(0.2)
+
+
 @pytest.fixture(autouse=True, scope="session")
 def lift_pass_pricing_app():
     """ starts the lift pass pricing flask app running on localhost """
     p = multiprocessing.Process(target=server, args=(TEST_PORT,))
     p.start()
-    # we need to give it time to start - one second usually seems to be enough
-    time.sleep(1)
-    yield f"http://127.0.0.1:{TEST_PORT}"
+    server_url = f"http://127.0.0.1:{TEST_PORT}"
+    wait_for_server_to_start(server_url)
+    yield server_url
     p.terminate()
 
 
